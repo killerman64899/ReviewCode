ee3ae95a87cc296bd88d73f5fd27dc73bda9c9b4
killerman64899 <76467006+killerman64899@users.noreply.github.com>
2025-05-28 22:35:26 +0530

    Update Startup.java

--- Startup.java
+++ Startup.java
@@ -1,33 +1,39 @@
-package Startups;
-
 import java.util.ArrayList;
-
-public class Startup {
+public class Startup{
     private String name;
-    private ArrayList<String> locations;
-    private boolean isAlive = false;
+    private ArrayList<String> location = new ArrayList(3);
+    
+    Startup(String startupName){
+        setName(startupName);
+    }
 
     public void setName(String n){
         name = n;
     }
-
     public String getName(){
         return name;
     }
-
-    public void setLocations(ArrayList<String> locs){
-        locations = locs;
+    public void setLocation(ArrayList<String> loc){
+        location = loc;
+    }
+    public ArrayList<String> getLocation(){
+        return location;
     }
 
-    public ArrayList<String> getLocations(){
-        return locations;
-    }
-
-    public void setLifeState(boolean trueOrFalse){
-        isAlive = trueOrFalse;
-    }
-
-    public boolean getLifeState(){
-        return isAlive;
+    public String checkYourself(String userInput){
+        String toReturn = "miss";
+        for(String pos: location){
+            if(userInput.equals(pos)){
+                toReturn = "hit";
+                location.remove(pos);
+                break;
+            }   
+        }
+        if(location.isEmpty()){
+            toReturn="kill";
+            System.out.println("You sunk "+ name+ "!");
+        }
+        return toReturn;
+        
     }
 }
