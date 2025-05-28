37e4f2e3c44429527fe97b123fcfa1e342f96a83
killerman64899 <76467006+killerman64899@users.noreply.github.com>
2025-05-28 22:35:13 +0530

    Update StartupBust.java

--- StartupBust.java
+++ StartupBust.java
@@ -1,122 +1,100 @@
-package Startups;
-
 import java.util.ArrayList;
+public class StartupBust{
+    private GameHelper ghelper = new GameHelper();
+    private ArrayList<Startup> startups = new ArrayList();
+    private int numOfGuesses = 0;
 
-public class StartupBust {
-    private GameHelper helper;
-    private ArrayList<Startup> startups = new ArrayList<Startup>(3);
-    private int numOfGuesses;
+    StartupBust(){
+        this.setUpGame();
+        this.startPlaying();
+    }
+    
+    private void setUpGame() {
+        Startup s1 = new Startup("Bro1");
+        Startup s2 = new Startup("Bro2");
+        Startup s3 = new Startup("Bro3");
+        startups.add(s1);
+        startups.add(s2);
+        startups.add(s3);
+        System.out.println("Your goal is to sink the three startups by typing out their coordinates.");
+        System.out.println("The grid has 7 rows and 7 columns, where the rows range from 1-7 and columns range from A-G");
+        System.out.println("Example coordinate: J8");
+        System.out.println("Good Luck!");
+        
+        for(Startup startup: startups){
+            while(true){
+                boolean conflict = false;
+                ArrayList<String> locationToSet = ghelper.generateLocation(3);
+                for(Startup s: startups){
+                    if (s.getLocation() == null || s.getLocation().isEmpty()) {continue;}
 
-    void setUpGame() {
-        Startup STARTUPone = new Startup();
-        Startup STARTUPtwo = new Startup();
-        Startup STARTUPthree = new Startup();
-
-        STARTUPone.setName("FIITJEE");
-        STARTUPtwo.setName("Akaash");
-        STARTUPthree.setName("Byjus");
-
-        startups.add(STARTUPone);
-        startups.add(STARTUPtwo);
-        startups.add(STARTUPthree);
-
-        helper = new GameHelper();
-
-        for (int i = 0; i != 3; i++) {
-            if (i == 0) {
-                ArrayList<String> locationsStartup1 = helper.placeStartup();
-                STARTUPone.setLocations(locationsStartup1);
-                continue;
-            }
-            if (i == 1) {
-                while (true) {
-                    ArrayList<String> locationsStartup2 = helper.placeStartup();
-
-                    ArrayList<String> locationsStartup1 = STARTUPone.getLocations();
-                    if (locationsStartup2.contains(locationsStartup1.get(0)) == false
-                            && locationsStartup2.contains(locationsStartup1.get(1)) == false
-                            && locationsStartup2.contains(locationsStartup1.get(2)) == false) {
-                        STARTUPtwo.setLocations(locationsStartup2);
-                        break;
+                    ArrayList<String> locationToCompare= s.getLocation();                 
+                    for(String pos1:locationToCompare){
+                        for(String pos2: locationToSet){
+                            if(pos1.equals(pos2)){
+                                conflict=true;
+                                break;
+                            }
+                        }
+                        if(conflict){break;}
                     }
+                    if(conflict){break;}
                 }
-                continue;
-            }
-            if (i == 2) {
-                while (true) {
-                    ArrayList<String> locationsStartup3 = helper.placeStartup();
-
-                    ArrayList<String> locationsStartup1 = STARTUPone.getLocations();
-                    ArrayList<String> locationsStartup2 = STARTUPtwo.getLocations();
-                    if (locationsStartup3.contains(locationsStartup1.get(0)) == false
-                            && locationsStartup3.contains(locationsStartup1.get(1)) == false
-                            && locationsStartup3.contains(locationsStartup1.get(2)) == false
-                            && locationsStartup3.contains(locationsStartup2.get(0)) == false
-                            && locationsStartup3.contains(locationsStartup2.get(1)) == false
-                            && locationsStartup3.contains(locationsStartup2.get(2)) == false) {
-                        STARTUPthree.setLocations(locationsStartup3);
-                        break;
-                    }
+                if(!conflict){
+                    startup.setLocation(locationToSet);
+                    break;
                 }
-                continue;
             }
         }
-        for(Startup s : startups){
-            s.setLifeState(true);
+
+        for(Startup startup:startups){
+            System.out.println("Startup "+ startup.getName() + " was placed at:");
+            for(String pos: startup.getLocation()){
+                System.out.println(pos);
+            }
         }
     }
 
-    void startPlaying() {
-        String guess;
-        System.out.println("Your goal is to sink all the startups in the least number of guesses");
-        System.out.println("Have a go!");
-
-        while (startups.get(0).getLifeState() == true
-                || startups.get(1).getLifeState() == true
-                || startups.get(2).getLifeState() == true) {
-            String message = "Make a guess";
-            guess = helper.getUserInput(message);
+    private void startPlaying(){
+        while(!startups.isEmpty()){
+            String guess = ghelper.getUserInput("Enter a guess");
             checkUserGuess(guess);
         }
         finishGame();
     }
 
-    void checkUserGuess(String userGuess) {
+    private void checkUserGuess(String guess){
         numOfGuesses++;
-        String result = "Miss";
-        for (Startup startupToCheck : startups) {
-            ArrayList<String> locationsOfStartup = startupToCheck.getLocations();
-            if (startupToCheck.getLifeState() == true) {
-                if (locationsOfStartup.contains(userGuess)) {
-                    result = "Hit";
-                    locationsOfStartup.remove(locationsOfStartup.indexOf(userGuess));
-                    if (locationsOfStartup.isEmpty()) {
-                        result = "You sunk "+startupToCheck.getName()+"!";
-                        startupToCheck.setLifeState(false);
-                    }
-                }
-            }
+        String result = "miss";
 
+        for(Startup s: startups){
+            result = s.checkYourself(guess);
+            if(result.equals("hit")){
+                break;
+            } else if(result.equals("kill")){
+                startups.remove(s);
+                break;
+            }
         }
         System.out.println(result);
-    }
 
-    void finishGame() {
-        System.out.println("Game over! You wrecked all the startups! It took you " + numOfGuesses + " guesses.");
-        if (numOfGuesses > 40) {
-            System.out.println("Bro made sure to nuke every survivor");
-        } else if (numOfGuesses < 30 && numOfGuesses > 20) {
-            System.out.println("You're learning.. i guess?");
-        } else if (numOfGuesses < 20 && numOfGuesses > 15) {
-            System.out.println("Wow! You're pretty good eh?");
-        } else if (numOfGuesses < 15) {
-            System.out.println("Chill out bruh there's no need to speedrun this");
+    }
+    private void finishGame(){
+        System.out.println("Game over!");
+        System.out.println("You took "+ numOfGuesses + " Guesses!");
+        if(numOfGuesses>=18){
+            System.out.println("Zero thoughts in that brain");
+        } else if (numOfGuesses>= 10){
+            System.out.println("Good boy");
+        } else if (numOfGuesses==9){
+            System.out.println("Perfectionist ahh");
+        } else {
+            System.out.println("Wtf");
         }
     }
 
-    public static void main(String[] args) {
-        StartupBust game = new StartupBust();
-        game.setUpGame();
-        game.startPlaying();
+    public static void main(String[] args){
+        new StartupBust();
     }
+
 }
