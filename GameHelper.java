c856af7ecd3eb588eef841c344f870b42fc72756
killerman64899 <76467006+killerman64899@users.noreply.github.com>
2025-05-28 22:35:36 +0530

    Update GameHelper.java

--- GameHelper.java
+++ GameHelper.java
@@ -1,285 +1,39 @@
-package Startups;
+import java.util.*;
+public class GameHelper{
+    private static final String ALPHABET = "ABCDEFG";
+    private static final char[] alphaArray = ALPHABET.toCharArray();
+    private final Random random = new Random();
+    private final Scanner scanner = new Scanner(System.in);
 
-import java.util.ArrayList;
-import java.util.Random;
-import java.util.Scanner;
-
-public class GameHelper {
-    ArrayList<String> placeStartup() {
-        ArrayList<String> finalPositions = new ArrayList<String>(3);
-        Random random = new Random();
-        String finalPos1, finalPos2, finalPos3;
-        String Y1 = "Null", Y2 = "Null", Y3 = "Null", X1 = "Null", X2 = "Null", X3 = "Null";
-        byte oldhorizontalPos;
-        char horizontalPos;
-        byte verticalPos;
-        boolean staySameXRow;
-
-        boolean firstVerticalPos = false, lastVerticalPos = false, firstHorizontalPos = false, lastHorizontalPos = false;
-
-        oldhorizontalPos = (byte) random.nextInt(0, 6);
-        verticalPos = (byte) random.nextInt(1, 7);
-        staySameXRow = random.nextBoolean();
-
-        switch (oldhorizontalPos) {
-            case 0:
-                horizontalPos = 'A';
-                break;
-            case 1:
-                horizontalPos = 'B';
-                break;
-            case 2:
-                horizontalPos = 'C';
-                break;
-            case 3:
-                horizontalPos = 'D';
-                break;
-            case 4:
-                horizontalPos = 'E';
-                break;
-            case 5:
-                horizontalPos = 'F';
-                break;
-            case 6:
-                horizontalPos = 'G';
-                break;
-            default:
-                horizontalPos = 'Z';
-                System.out.println("Error in switch case GameHelper.placeStartup()");
-                break;
-        }
-
-        if (verticalPos == 1) {
-            firstVerticalPos = true;
-        } else if (verticalPos == 7) {
-            lastVerticalPos = true;
-        }
-        if (horizontalPos == 'A') {
-            firstHorizontalPos = true;
-        } else if (horizontalPos == 'G') {
-            lastHorizontalPos = true;
-        }
-
-        //checks
-        if(firstHorizontalPos){
-            lastHorizontalPos = false;
-        } else if (lastHorizontalPos){
-            firstHorizontalPos = false;
-        }
-        if(firstVerticalPos){
-            lastVerticalPos = false;
-        } else if (lastVerticalPos){
-            firstVerticalPos = false;
-        }
-        
-
-        if (firstHorizontalPos == false && lastHorizontalPos == false && firstVerticalPos == false && lastVerticalPos == false ) {
-            // then we can take it as mid pos
-            X2 = String.valueOf(horizontalPos);
-            Y2 = String.valueOf(verticalPos);
-        
-            if(staySameXRow==true){
-                X1 = String.valueOf(horizontalPos);
-                Y1 = String.valueOf(verticalPos - 1);
-
-                X3 = String.valueOf(horizontalPos);
-                Y3 = String.valueOf(verticalPos + 1);
-            } else {
-                X1 = String.valueOf((char) (horizontalPos - 1));
-                Y1 = String.valueOf(verticalPos);
-
-                X3 = String.valueOf((char) (horizontalPos + 1));
-                Y3 = String.valueOf(verticalPos);
+    public ArrayList<String> generateLocation(int length){
+        boolean horizontalOrientation = random.nextBoolean();
+        ArrayList<String> positions = new ArrayList(length);
+        int xStart, yStart; char yStartAlpha; String coords;
+        if(horizontalOrientation){
+            xStart = random.nextInt(0,8-length);
+            yStart = random.nextInt(0,7);
+            yStartAlpha = alphaArray[yStart];
+            String y = String.valueOf(yStartAlpha);
+            for(int i = 0; i<length; i++){
+                coords = y + String.valueOf(xStart+1+i) ;
+                positions.add(coords);
             }
-        } else if (firstHorizontalPos == false && lastHorizontalPos == false && firstVerticalPos == true && lastVerticalPos == false) {
-            if(staySameXRow==true){
-                //then we can take it as the first pos on the edge of the bottom x row edge
-                X1 = String.valueOf(horizontalPos);
-                Y1 = String.valueOf(verticalPos);
-                
-                X2 = String.valueOf(horizontalPos);
-                Y2 = String.valueOf(verticalPos + 1);
-
-                X3 = String.valueOf(horizontalPos);
-                Y3 = String.valueOf(verticalPos + 2);                
-            } else{
-                //then we can take it as the mid pos on the bottom X row edge
-                X1 = String.valueOf((char) (horizontalPos - 1));
-                Y2 = String.valueOf(verticalPos);
-
-                X2 = String.valueOf(horizontalPos);
-                Y1 = String.valueOf(verticalPos);
-
-                X3 = String.valueOf((char) (horizontalPos + 1));
-                Y3 = String.valueOf(verticalPos);
-            }
-        } else if (firstHorizontalPos == false && lastHorizontalPos == false && firstVerticalPos == false && lastVerticalPos == true){
-            if(staySameXRow==true){
-                //then we can take it as the first pos on the edge of the top x row edge
-                X1 = String.valueOf(horizontalPos);
-                Y1 = String.valueOf(verticalPos);
-                
-                X2 = String.valueOf(horizontalPos);
-                Y2 = String.valueOf(verticalPos - 1);
-
-                X3 = String.valueOf(horizontalPos);;
-                Y3 = String.valueOf(verticalPos - 2);                
-            } else{
-                //then we can take it as the mid pos on the top X row edge
-                X1 = String.valueOf((char) (horizontalPos - 1));
-                Y1 = String.valueOf(verticalPos);
-
-                X2 = String.valueOf(horizontalPos);
-                Y2 = String.valueOf(verticalPos);
-
-                X3 = String.valueOf((char) (horizontalPos + 1));
-                Y3 = String.valueOf(verticalPos);
-            }
-        } else if (firstHorizontalPos == true && lastHorizontalPos == false && firstVerticalPos == false && lastVerticalPos == false){
-            if(staySameXRow==true){
-                //then we can take it as the mid pos on the edge of the first y row edge
-                X1 = String.valueOf(horizontalPos);
-                Y1 = String.valueOf(verticalPos - 1);
-                
-                X2 = String.valueOf(horizontalPos);
-                Y2 = String.valueOf(verticalPos);
-
-                X3 = String.valueOf(horizontalPos);;
-                Y3 = String.valueOf(verticalPos + 1);                
-            } else{
-                //then we can take it as the first pos on the first y row edge
-                X1 = String.valueOf(horizontalPos);
-                Y1 = String.valueOf(verticalPos);
-
-                X2 = String.valueOf((char) (horizontalPos + 1));
-                Y2 = String.valueOf(verticalPos);
-
-                X3 = String.valueOf((char) (horizontalPos + 2));
-                Y3 = String.valueOf(verticalPos);
-            }
-        }else if (firstHorizontalPos == false && lastHorizontalPos == true && firstVerticalPos == false && lastVerticalPos == false){
-            if(staySameXRow==true){
-                //then we can take it as the mid pos on the edge of the last y row edge
-                X1 = String.valueOf(horizontalPos);
-                Y1 = String.valueOf(verticalPos - 1);
-                
-                X2 = String.valueOf(horizontalPos);
-                Y2 = String.valueOf(verticalPos);
-
-                X3 = String.valueOf(horizontalPos);;
-                Y3 = String.valueOf(verticalPos + 1);                
-            } else{
-                //then we can take it as the first pos on the last y row edge
-                X1 = String.valueOf(horizontalPos);
-                Y1 = String.valueOf(verticalPos);
-
-                X2 = String.valueOf((char) (horizontalPos - 1));
-                Y2 = String.valueOf(verticalPos);
-
-                X3 = String.valueOf((char) (horizontalPos - 2));
-                Y3 = String.valueOf(verticalPos);
-            }
-        } else if(firstHorizontalPos == true && lastHorizontalPos == false && firstVerticalPos == true && lastVerticalPos == false){
-            //then we can take it as the first pos on the corner of the first x and first y row
-            if(staySameXRow==true){
-                X1 = String.valueOf(horizontalPos);
-                Y1 = String.valueOf(verticalPos);
-                
-                X2 = String.valueOf(horizontalPos);
-                Y2 = String.valueOf(verticalPos + 1);
-
-                X3 = String.valueOf(horizontalPos);;
-                Y3 = String.valueOf(verticalPos + 2);                
-            } else{
-                X1 = String.valueOf(horizontalPos);
-                Y1 = String.valueOf(verticalPos);
-
-                X2 = String.valueOf((char) (horizontalPos + 1));
-                Y2 = String.valueOf(verticalPos);
-
-                X3 = String.valueOf((char) (horizontalPos + 2));
-                Y3 = String.valueOf(verticalPos);
-            }
-        } else if (firstHorizontalPos == true && lastHorizontalPos == false && firstVerticalPos == false && lastVerticalPos == true){
-            //then we can take it as the first pos on the corner of the first x and last y row
-            if(staySameXRow==true){
-                X1 = String.valueOf(horizontalPos);
-                Y1 = String.valueOf(verticalPos);
-                
-                X2 = String.valueOf(horizontalPos);
-                Y2 = String.valueOf(verticalPos - 1);
-
-                X3 = String.valueOf(horizontalPos);;
-                Y3 = String.valueOf(verticalPos - 2);                
-            } else{
-                X1 = String.valueOf(horizontalPos);
-                Y1 = String.valueOf(verticalPos);
-
-                X2 = String.valueOf((char) (horizontalPos + 1));
-                Y2 = String.valueOf(verticalPos);
-
-                X3 = String.valueOf((char) (horizontalPos + 2));
-                Y3 = String.valueOf(verticalPos);
-            }
-        } else if (firstHorizontalPos == false && lastHorizontalPos == true && firstVerticalPos == true && lastVerticalPos == false){
-            //then we can take it as the first pos on the corner of the last x and first y row
-            if(staySameXRow==true){
-                X1 = String.valueOf(horizontalPos);
-                Y1 = String.valueOf(verticalPos);
-                
-                X2 = String.valueOf(horizontalPos);
-                Y2 = String.valueOf(verticalPos + 1);
-
-                X3 = String.valueOf(horizontalPos);;
-                Y3 = String.valueOf(verticalPos + 2);                
-            } else{
-                X1 = String.valueOf(horizontalPos);
-                Y1 = String.valueOf(verticalPos);
-
-                X2 = String.valueOf((char) (horizontalPos - 1));
-                Y2 = String.valueOf(verticalPos);
-
-                X3 = String.valueOf((char) (horizontalPos - 2));
-                Y3 = String.valueOf(verticalPos);
-            }
-        } else if (firstHorizontalPos == false && lastHorizontalPos == true && firstVerticalPos == false && lastVerticalPos == true){
-            //then we can take it as the first pos on the corner of the last x and last y row
-            if(staySameXRow==true){
-                X1 = String.valueOf(horizontalPos);
-                Y1 = String.valueOf(verticalPos);
-                
-                X2 = String.valueOf(horizontalPos);
-                Y2 = String.valueOf(verticalPos - 1);
-
-                X3 = String.valueOf(horizontalPos);;
-                Y3 = String.valueOf(verticalPos - 2);                
-            } else{
-                X1 = String.valueOf(horizontalPos);
-                Y1 = String.valueOf(verticalPos);
-
-                X2 = String.valueOf((char) (horizontalPos - 1));
-                Y2 = String.valueOf(verticalPos);
-
-                X3 = String.valueOf((char) (horizontalPos - 2));
-                Y3 = String.valueOf(verticalPos);
+        } else {
+            xStart = random.nextInt(0,7);
+            yStart = random.nextInt(0,8-length);
+            String x = String.valueOf(xStart+1);
+            for(int i = 0; i<length; i++){
+                yStartAlpha = alphaArray[yStart+i];
+                coords = String.valueOf(yStartAlpha) + x;
+                positions.add(coords);
             }
         }
-
-        finalPos1 = X1 + Y1;
-        finalPos2 = X2 + Y2;
-        finalPos3 = X3 + Y3;
-
-        finalPositions.add(finalPos1);
-        finalPositions.add(finalPos2);
-        finalPositions.add(finalPos3);
-
-        return finalPositions;
+        return positions;
     }
 
-    public String getUserInput(String message){
-        Scanner sc = new Scanner(System.in);
-        System.out.println(message);
-        String userInput = sc.next();
-        return userInput;
+    public String getUserInput(String displayText){
+        System.out.println(displayText);
+        String input = scanner.nextLine();
+        return input.toUpperCase();
     }
 }
